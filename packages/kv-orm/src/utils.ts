62a5b4779e34e3876a16dabb87b1bb4ee7963f7d
Greg Brimble <developer@gregbrimble.com>
2019-04-20 10:37:01 +0100

    chore: Finish Spring hiatus

--- packages/kv-orm/src/utils.ts
+++ packages/kv-orm/src/utils.ts
@@ -11,3 +11,11 @@ export function isRelatedInstanceOf(instance: any, target: any): boolean {
 export function uuid(): string {
   return uuidv4();
 }
+
+export function hexToArrayBuffer(hexString: string): ArrayBuffer {
+  return new Uint8Array(hexString.match(/[\da-f]{2}/gi)!.map((hexNumber) => parseInt(hexNumber, 16))).buffer;
+}
+
+export function arrayBufferToHex(arrayBuffer: ArrayBuffer): string {
+  return '';
+}
