6cdfdc7055a154ab61d24dbf15c79364fbc16f65
Greg Brimble <developer@gregbrimble.com>
2019-07-26 19:59:45 +0100

    Stale Local WIP

---
+++ packages/kv-orm/src/relationships/OneToMany.ts
@@ -0,0 +1,32 @@
+import { BaseEntity, Datastore } from '..';
+
+export function OneToMany(type: typeof BaseEntity) {
+  return (target: object, key: string) => {
+    const constructor = target.constructor as any;
+    constructor.properties = constructor.properties || [];
+    (constructor.properties as object[]).push({
+      key,
+      type: OneToMany
+    });
+
+    // @ts-ignore
+    if (delete this[key]) {
+      Object.defineProperty(target, key, {
+        enumerable: true,
+        async get(this: any): Promise<any> {
+          const datastore = this.constructor.datastore as Datastore;
+          const relationUUID = await datastore.read(datastore.generateKey(this, key));
+          if (relationUUID) {
+            return await type.get(relationUUID);
+          } else {
+            return Promise.resolve(null);
+          }
+        },
+        set(this: any, value: BaseEntity) {
+          const datastore = this.constructor.datastore as Datastore;
+          datastore.write(datastore.generateKey(this, key), value.uuid);
+        }
+      })
+    }
+  }
+}
