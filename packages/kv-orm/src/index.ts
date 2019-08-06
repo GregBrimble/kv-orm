feea82b50fb646448cd3f73dd92e27a230c20230
Greg Brimble <developer@gregbrimble.com>
2019-08-06 22:47:25 +0100

    Export Relationships

--- packages/kv-orm/src/index.ts
+++ packages/kv-orm/src/index.ts
@@ -2,3 +2,7 @@ export * from './Entity';
 export * from './columns/Column';
 export * from './columns/UUIDColumn';
 export * from './datastore/Datastore';
+export * from './relationships/OneToOne';
+export * from './relationships/OneToMany';
+export * from './relationships/ManyToOne';
+export * from './relationships/ManyToMany';
